11bfa5fb73e259867139883923e1d16a535790d8
Matias CR <49898779+matiascr@users.noreply.github.com>
2023-10-05 23:48:15 +0200

    Callbacks and widgets (#3)
    * Added support for PSO callbacks and widgets
    
    * Added test for callbacks
    
    * Added example notebook
    
    * Renamed coeff
    
    * Fix global best not being update
    
    * Removed empty notebooks and updated example
    
    * Tuned parameters and added explanations
    
    * Added explanations and reordered steps
    
    * Refactored for readability and added comments

--- lib/pso/particle.ex
+++ lib/pso/particle.ex
@@ -17,15 +17,18 @@ defmodule PSO.Particle do
 
   defp initialize_particle(pso_args) do
     pso_args
+    # Initialize the particle's position with a uniformly distributed random vector
     |> Map.merge(initialize_position(pso_args.dimensions, pso_args.bound_up, pso_args.bound_down))
+    # Initialize the particle's velocity
     |> Map.merge(initialize_velocity(pso_args.dimensions, pso_args.bound_up, pso_args.bound_down))
+    # Initialize the particle's best known position to its initial position
     |> then(&Map.merge(&1, %{personal_best: &1.position}))
   end
 
   defp initialize_position(dimensions, bound_up, bound_down) do
     initial_position = random_uniform_tensor(dimensions, bound_down, bound_up)
 
-    %{position: initial_position, personal_best: initial_position}
+    %{position: initial_position}
   end
 
   defp initialize_velocity(dimensions, bound_up, bound_down) do
@@ -43,15 +46,16 @@ defmodule PSO.Particle do
 
   @impl true
   def handle_call({:move, global_best}, _from, state) do
+    # Pick random numbers
     random_p = random_uniform_tensor(state.dimensions)
     random_g = random_uniform_tensor(state.dimensions)
 
-    new_velocity = update_velocity(Map.drop(state, [:fun]), random_p, random_g, global_best)
-
     new_position =
       state.position
-      |> update_position(new_velocity)
+      |> update_position(state.velocity)
       |> bound_position(state.bound_up, state.bound_down)
+
+    new_velocity = update_velocity(Map.drop(state, [:fun]), random_p, random_g, global_best)
 
     personal_best =
       if state.fun.(state.position) > state.fun.(new_position),
@@ -63,6 +67,7 @@ defmodule PSO.Particle do
       |> Map.merge(%{velocity: new_velocity})
       |> Map.merge(%{position: new_position})
       |> Map.merge(%{personal_best: personal_best})
+      |> Map.merge(%{global_best: global_best})
 
     {:reply, personal_best, new_state}
   end
@@ -79,8 +84,8 @@ defmodule PSO.Particle do
 
   defnp update_velocity(state, random_p, random_g, global_best) do
     (state.inertia * state.velocity)
-    |> Nx.add(state.coef_p * random_p * (state.personal_best - state.position))
-    |> Nx.add(state.coef_g * random_g * (global_best - state.position))
+    |> Nx.add(state.coeff_p * random_p * (state.personal_best - state.position))
+    |> Nx.add(state.coeff_g * random_g * (global_best - state.position))
   end
 
   defnp update_position(position, velocity) do
