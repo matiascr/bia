c117f5c1b6d154185e183706c769a1d2cc10c733
Matias CR <carlander.matias@gmail.com>
2023-11-12 18:21:04 +0100

    Updated mix to add notebook to docs

--- mix.exs
+++ mix.exs
@@ -17,14 +17,12 @@ defmodule Bia.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.30", only: :dev, runtime: false},
@@ -36,8 +34,12 @@ defmodule Bia.MixProject do
 
   defp docs do
     [
-      main: "Bia",
+      main: "readme",
       source_url: @source_url,
+      extra_section: "Guides",
+      extras: [
+        "notebooks/pso_example.livemd"
+      ],
       groups_for_modules: [
         Heuristics: [
           PSO
@@ -58,17 +60,36 @@ defmodule Bia.MixProject do
 
   defp before_closing_body_tag(:html) do
     """
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/katex.min.css" integrity="sha384-beuqjL2bw+6DBM2eOpr5+Xlw+jiH44vMdVQwKxV28xxpoInPHTVmSvvvoPq9RdSh" crossorigin="anonymous">
-    <script defer="" src="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/katex.min.js" integrity="sha384-aaNb715UK1HuP4rjZxyzph+dVss/5Nx3mLImBe9b0EW4vMUkc1Guw4VRyQKBC0eG" crossorigin="anonymous"></script>
-    <script defer="" src="https://cdn.jsdelivr.net/npm/katex@0.13.19/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous" onload="renderMathInElement(document.body);"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css" integrity="sha384-t5CR+zwDAROtph0PXGte6ia8heboACF9R5l/DiY+WZ3P2lxNgvJkQk5n7GPvLMYw" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.js" integrity="sha384-FaFLTlohFghEIZkw6VGwmf9ISTubWAVYW8tG8+w2LAIftJEULZABrF9PPFv+tVkH" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/contrib/auto-render.min.js" integrity="sha384-bHBqxz8fokvgoJ/sc17HODNxa42TlaEhB+w8ZJXTc2nZf1VgEaFZeZvT4Mznfz0v" crossorigin="anonymous"></script>
     <script>
       document.addEventListener("DOMContentLoaded", function() {
         renderMathInElement(document.body, {
           delimiters: [
-              { left: "$$", right: "$$", display: true },
-              { left: "$", right: "$", display: false },
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
           ]
         });
+      });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/vega@5.20.2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-lite@5.1.1"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.18.2"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        for (const codeEl of document.querySelectorAll("pre code.vega-lite")) {
+          try {
+            const preEl = codeEl.parentElement;
+            const spec = JSON.parse(codeEl.textContent);
+            const plotEl = document.createElement("div");
+            preEl.insertAdjacentElement("afterend", plotEl);
+            vegaEmbed(plotEl, spec);
+            preEl.remove();
+          } catch (error) {
+            console.log("Failed to render Vega-Lite plot: " + error)
+          }
+        }
       });
     </script>
     """
